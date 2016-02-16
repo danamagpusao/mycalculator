ca93673576fd6b96685e8805d3f3ade425d11933
danamagpusao <danamagpusao818@gmail.com>
2016-02-16 15:22:41 +0800

    test case added

---
+++ MyTester.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MyTester {
+
+	@Test
+	public void testNfactorial() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBinarySearch() {
+		fail("Not yet implemented");
+	}
+
+}
